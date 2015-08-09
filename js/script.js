ce11cd5fbeca2334ffcb59198cdc62bddf0bb48b
liteway <liteway@126.com>
2015-08-09 15:24:37 +0800

    Site updated: 2015-08-09 15:24:37

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
 (function($){
     
+    $('#totop').hide();
    /*toTop start*/
-  // When to show the scroll link
-  // higher number = scroll link appears further down the page
-  var upperLimit = 1000;
-  // Our scroll link element
-  var scrollElem = $('#totop');
-  // Scroll to top speed
-  var scrollSpeed = 500;
-  // Show and hide the scroll to top link based on scroll position
-  $(window).scroll(function() {
-    var scrollTop = $(document).scrollTop();
-    if (scrollTop > upperLimit) {
-      $(scrollElem).stop().fadeTo(300, 1); // fade back in
-    } else {
-      $(scrollElem).stop().fadeTo(300, 0); // fade out
-    }
-  });
+    if ($('#sidebar').hasClass('fixed')) {
+        var ratio = $('#sidebar').width()/$('#sidebar').parent().width();
+        $('#sidebar').width(
+          $('#sidebar').width() 
+          - 2*parseFloat($('#sidebar').parent().css('padding-right'))*ratio);
+      }
 
-  // Scroll to top animation on click
-  $(scrollElem).click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, scrollSpeed);
-    return false;
-  });
+      $('#wrap').scroll(function(){
+          var offset = $(this).scrollTop();
+          $('#sidebar.fixed').css('top', Math.max(offset-100, 0));
+          if (offset > $('#sidebar').height() && !$('#sidebar').hasClass('fixed')) {
+            $('#totop').show();
+          } else {
+            $('#totop').hide();
+          }
+      });
   /*toTop end*/
    
   // Search
